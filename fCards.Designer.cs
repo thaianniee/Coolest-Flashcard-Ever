fecbdbfb9fae6ee105e0c6edfa3d46e483c55d22
Thái An Phan <thaianniee@gmail.com>
2021-08-26 20:36:04 +0700

    rv

---
+++ fCards.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+using System.Drawing;
+
+namespace CoolestFlashCardAppEver
+{
+    partial class fCards
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fCards));
+            this.lblEng = new System.Windows.Forms.Label();
+            this.lblVi = new System.Windows.Forms.Label();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btCustomize = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEng
+            // 
+            resources.ApplyResources(this.lblEng, "lblEng");
+            this.lblEng.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblEng.Name = "lblEng";
+            // 
+            // lblVi
+            // 
+            resources.ApplyResources(this.lblVi, "lblVi");
+            this.lblVi.ForeColor = System.Drawing.Color.Navy;
+            this.lblVi.Name = "lblVi";
+            // 
+            // btClose
+            // 
+            this.btClose.BackgroundImage = global::CoolestFlashCardAppEver.Properties.Resources.delete;
+            resources.ApplyResources(this.btClose, "btClose");
+            this.btClose.ForeColor = System.Drawing.Color.Black;
+            this.btClose.Name = "btClose";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // btCustomize
+            // 
+            this.btCustomize.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btCustomize.BackgroundImage = global::CoolestFlashCardAppEver.Properties.Resources.settings;
+            resources.ApplyResources(this.btCustomize, "btCustomize");
+            this.btCustomize.ForeColor = System.Drawing.Color.Black;
+            this.btCustomize.Name = "btCustomize";
+            this.btCustomize.UseVisualStyleBackColor = false;
+            this.btCustomize.Click += new System.EventHandler(this.btCustomize_Click);
+            // 
+            // btEdit
+            // 
+            this.btEdit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btEdit.BackgroundImage = global::CoolestFlashCardAppEver.Properties.Resources.pencil_128x128;
+            resources.ApplyResources(this.btEdit, "btEdit");
+            this.btEdit.ForeColor = System.Drawing.Color.Black;
+            this.btEdit.Name = "btEdit";
+            this.btEdit.UseVisualStyleBackColor = false;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // fCards
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ControlBox = false;
+            this.Controls.Add(this.btEdit);
+            this.Controls.Add(this.btCustomize);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.lblVi);
+            this.Controls.Add(this.lblEng);
+            this.ForeColor = System.Drawing.Color.Aquamarine;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fCards";
+            this.ShowInTaskbar = false;
+            this.TransparencyKey = System.Drawing.Color.DarkSalmon;
+            this.Load += new System.EventHandler(this.fCards_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblEng;
+        public System.Windows.Forms.Label lblVi;
+        public System.Windows.Forms.Button btClose;
+        public System.Windows.Forms.Button btCustomize;
+        public System.Windows.Forms.Button btEdit;
+    }
+}
+
---
+++ fCustomize.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace CoolestFlashCardAppEver
+{
+    partial class fCustomize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSave = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.lblBackColor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVi = new System.Windows.Forms.Label();
+            this.txbBackColor1 = new System.Windows.Forms.TextBox();
+            this.txbEngColor1 = new System.Windows.Forms.TextBox();
+            this.txbViColor1 = new System.Windows.Forms.TextBox();
+            this.txbBackColor2 = new System.Windows.Forms.TextBox();
+            this.txbEngColor2 = new System.Windows.Forms.TextBox();
+            this.txbViColor2 = new System.Windows.Forms.TextBox();
+            this.txbBackColor3 = new System.Windows.Forms.TextBox();
+            this.txbEngColor3 = new System.Windows.Forms.TextBox();
+            this.txbViColor3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbTime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btSave
+            // 
+            this.btSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(116)))), ((int)(((byte)(134)))));
+            this.btSave.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(116)))), ((int)(((byte)(134)))));
+            this.btSave.FlatAppearance.BorderSize = 0;
+            this.btSave.ForeColor = System.Drawing.Color.White;
+            this.btSave.Location = new System.Drawing.Point(395, 274);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(86, 34);
+            this.btSave.TabIndex = 0;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = false;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(116)))), ((int)(((byte)(134)))));
+            this.btCancel.ForeColor = System.Drawing.Color.White;
+            this.btCancel.Location = new System.Drawing.Point(269, 274);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(84, 34);
+            this.btCancel.TabIndex = 1;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = false;
+            this.btCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblBackColor
+            // 
+            this.lblBackColor.AutoSize = true;
+            this.lblBackColor.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblBackColor.ForeColor = System.Drawing.Color.White;
+            this.lblBackColor.Location = new System.Drawing.Point(12, 32);
+            this.lblBackColor.Name = "lblBackColor";
+            this.lblBackColor.Size = new System.Drawing.Size(133, 21);
+            this.lblBackColor.TabIndex = 2;
+            this.lblBackColor.Text = "Backgroud Color";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(64, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eng Color";
+            // 
+            // lblVi
+            // 
+            this.lblVi.AutoSize = true;
+            this.lblVi.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblVi.ForeColor = System.Drawing.Color.White;
+            this.lblVi.Location = new System.Drawing.Point(77, 136);
+            this.lblVi.Name = "lblVi";
+            this.lblVi.Size = new System.Drawing.Size(68, 21);
+            this.lblVi.TabIndex = 4;
+            this.lblVi.Text = "Vi Color";
+            // 
+            // txbBackColor1
+            // 
+            this.txbBackColor1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbBackColor1.Location = new System.Drawing.Point(174, 30);
+            this.txbBackColor1.Name = "txbBackColor1";
+            this.txbBackColor1.Size = new System.Drawing.Size(62, 27);
+            this.txbBackColor1.TabIndex = 5;
+            // 
+            // txbEngColor1
+            // 
+            this.txbEngColor1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbEngColor1.Location = new System.Drawing.Point(174, 82);
+            this.txbEngColor1.Name = "txbEngColor1";
+            this.txbEngColor1.Size = new System.Drawing.Size(62, 27);
+            this.txbEngColor1.TabIndex = 6;
+            // 
+            // txbViColor1
+            // 
+            this.txbViColor1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbViColor1.Location = new System.Drawing.Point(174, 134);
+            this.txbViColor1.Name = "txbViColor1";
+            this.txbViColor1.Size = new System.Drawing.Size(62, 27);
+            this.txbViColor1.TabIndex = 7;
+            // 
+            // txbBackColor2
+            // 
+            this.txbBackColor2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbBackColor2.Location = new System.Drawing.Point(279, 30);
+            this.txbBackColor2.Name = "txbBackColor2";
+            this.txbBackColor2.Size = new System.Drawing.Size(62, 27);
+            this.txbBackColor2.TabIndex = 8;
+            // 
+            // txbEngColor2
+            // 
+            this.txbEngColor2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbEngColor2.Location = new System.Drawing.Point(279, 82);
+            this.txbEngColor2.Name = "txbEngColor2";
+            this.txbEngColor2.Size = new System.Drawing.Size(62, 27);
+            this.txbEngColor2.TabIndex = 9;
+            // 
+            // txbViColor2
+            // 
+            this.txbViColor2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbViColor2.Location = new System.Drawing.Point(279, 134);
+            this.txbViColor2.Name = "txbViColor2";
+            this.txbViColor2.Size = new System.Drawing.Size(62, 27);
+            this.txbViColor2.TabIndex = 10;
+            // 
+            // txbBackColor3
+            // 
+            this.txbBackColor3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbBackColor3.Location = new System.Drawing.Point(395, 30);
+            this.txbBackColor3.Name = "txbBackColor3";
+            this.txbBackColor3.Size = new System.Drawing.Size(62, 27);
+            this.txbBackColor3.TabIndex = 11;
+            // 
+            // txbEngColor3
+            // 
+            this.txbEngColor3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbEngColor3.Location = new System.Drawing.Point(395, 82);
+            this.txbEngColor3.Name = "txbEngColor3";
+            this.txbEngColor3.Size = new System.Drawing.Size(62, 27);
+            this.txbEngColor3.TabIndex = 12;
+            // 
+            // txbViColor3
+            // 
+            this.txbViColor3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbViColor3.Location = new System.Drawing.Point(395, 134);
+            this.txbViColor3.Name = "txbViColor3";
+            this.txbViColor3.Size = new System.Drawing.Size(62, 27);
+            this.txbViColor3.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(99, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 21);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Time";
+            // 
+            // txbTime
+            // 
+            this.txbTime.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbTime.Location = new System.Drawing.Point(174, 183);
+            this.txbTime.Name = "txbTime";
+            this.txbTime.Size = new System.Drawing.Size(62, 27);
+            this.txbTime.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(242, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 21);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "seconds";
+            // 
+            // fCustomize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(502, 335);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbViColor3);
+            this.Controls.Add(this.txbEngColor3);
+            this.Controls.Add(this.txbBackColor3);
+            this.Controls.Add(this.txbViColor2);
+            this.Controls.Add(this.txbEngColor2);
+            this.Controls.Add(this.txbBackColor2);
+            this.Controls.Add(this.txbViColor1);
+            this.Controls.Add(this.txbEngColor1);
+            this.Controls.Add(this.txbBackColor1);
+            this.Controls.Add(this.lblVi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblBackColor);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fCustomize";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fCustomize";
+            this.Load += new System.EventHandler(this.fCustomize_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Label lblBackColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVi;
+        private System.Windows.Forms.TextBox txbBackColor1;
+        private System.Windows.Forms.TextBox txbEngColor1;
+        private System.Windows.Forms.TextBox txbViColor1;
+        private System.Windows.Forms.TextBox txbBackColor2;
+        private System.Windows.Forms.TextBox txbEngColor2;
+        private System.Windows.Forms.TextBox txbViColor2;
+        private System.Windows.Forms.TextBox txbBackColor3;
+        private System.Windows.Forms.TextBox txbEngColor3;
+        private System.Windows.Forms.TextBox txbViColor3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbTime;
+        private System.Windows.Forms.Label label3;
+    }
+}
